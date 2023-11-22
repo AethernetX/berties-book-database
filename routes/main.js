a3a8695535659db521dc4edff119e1c470c80464
Tam <mthanimr24@gmail.com>
2023-11-22 16:52:37 +0000

    commenting

--- routes/main.js
+++ routes/main.js
@@ -4,17 +4,19 @@ module.exports = function(app, shopData) {
     app.get('/',function(req,res){
         res.render('index.ejs', shopData)
     });
+    //about
     app.get('/about',function(req,res){
         res.render('about.ejs', shopData);
     });
+    //search
     app.get('/search',function(req,res){
         res.render("search.ejs", shopData);
     });
+    //search results after putting search
     app.get('/search-result', function (req, res) {
         //searching in the database
-        //checks which search method they want
         let sqlquery;
-
+        //checks which search method they want
         if(req.query.searchTypes == "advanced"){
             sqlquery = " SELECT * FROM books WHERE name LIKE '%" + req.query.keyword +"%'"; //query database to get all the books containing keyword
         }
@@ -32,13 +34,16 @@ module.exports = function(app, shopData) {
            res.render("search-result", newData);
         }); 
     });
+    //register
     app.get('/register', function (req,res) {
         res.render('register.ejs', shopData);                                                                     
-    });                                                                                                 
+    });       
+    //registered                                                                                          
     app.post('/registered', function (req,res) {
         // saving data in database
         res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
     }); 
+    //list
     app.get("/list", function(req, res){
         let sqlquery = "SELECT * FROM books"; //query database to get all the books
         // execute sql query
@@ -51,9 +56,11 @@ module.exports = function(app, shopData) {
            res.render("list.ejs", newData); 
         });
     });
+    //addbook
     app.get("/addbook", function(req,res){
         res.render("addbook.ejs", shopData);
     });
+    //takes you to this page when you add a book
     app.post('/bookadded', function (req,res) {
            // saving data in database
            let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
@@ -69,8 +76,8 @@ module.exports = function(app, shopData) {
              }
            });
      });    
-    
-   app.get("/bargainbooks", function(req, res){
+    //returns every book under £20
+    app.get("/bargainbooks", function(req, res){
         let sqlquery = "select name, price from books where price <= 20"; //query database to get all the books
         // execute sql query
         db.query(sqlquery, (err, result) => {
